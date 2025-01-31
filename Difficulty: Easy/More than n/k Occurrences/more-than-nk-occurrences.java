a93f9e6678f328ac209748b2711236db74f2d44c
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-01-31 14:18:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/More than n/k Occurrences/more-than-nk-occurrences.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.countOccurence(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int k) {
+        // your code here,return the answer
+        int n = arr.length;
+        int x = n / k;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int count = 0;
+        for (int key : map.keySet()) {
+            if (map.get(key) > x) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
