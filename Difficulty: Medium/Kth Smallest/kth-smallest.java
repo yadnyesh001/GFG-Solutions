e9586bce4aed79d2d39712875a363d6246dc77df
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-02-06 23:04:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int key = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            out.println(ob.kthSmallest(arr, key));
+            out.println("~");
+        }
+        out.flush();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        // Your code here
+        int max = 0;
+        for(int i=0; i<arr.length; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        int freq[] = new int[max+1];
+        for(int i=0; i<arr.length; i++){
+            freq[arr[i]]++;
+        }
+        int count = 0;
+        for(int i=0; i<freq.length; i++){
+            if(freq[i] > 0){
+                count++;
+                if(count == k){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
