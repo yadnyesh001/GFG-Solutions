c5b9aac78f2c667a1aba3338b1ad85400787d6b9
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-02-25 18:28:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Replace elements by its rank in the array/replace-elements-by-its-rank-in-the-array.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+      }
+      Solution obj = new Solution();
+      int ans[] = obj.replaceWithRank(a,n);
+      for(int e : ans)
+      System.out.print(e+" ");
+      System.out.println();
+    
+System.out.println("~");
+}
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int[] replaceWithRank(int arr[], int N) {
+        // code here
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        Map<Integer, Integer> rankMap = new HashMap<>();
+        int rank = 1;
+        for (int num : sortedArr) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank++);
+            }
+        }
+        int[] result = new int[N];
+        for (int i = 0; i < N; i++) {
+            result[i] = rankMap.get(arr[i]);
+        }
+        return result;
+    }
+}
