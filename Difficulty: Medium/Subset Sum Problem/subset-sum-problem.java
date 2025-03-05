eb29baa4534c8e3acc23694ef3b327a666e471a4
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-03-05 16:04:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = input_line.length;
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if (ob.isSubsetSum(arr, sum))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    static Boolean isSubsetSum(int arr[], int target) {
+        // code here
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; 
+
+        for(int i=0; i<arr.length; i++){
+            for (int j = target; j >= arr[i]; j--) {
+                dp[j] = dp[j] || dp[j - arr[i]];
+            }
+        }
+        return dp[target];
+    }
+}
