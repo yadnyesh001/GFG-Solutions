6aae8001459aa406d1376f28e3c4843bf631fa25
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-11-18 01:10:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,69 +1,25 @@
-//{ Driver Code Starts
-import java.util.*;
-
-// Driver code
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int testcases = sc.nextInt(); // Taking number of test cases as input
-
-        while (testcases-- > 0) {
-            int V = sc.nextInt(); // Number of vertices
-            int E = sc.nextInt(); // Number of edges
-
-            // Initialize adjacency list
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) {
-                adj.add(new ArrayList<>()); // Create a new list for each vertex
-            }
-
-            // Add edges to the adjacency list
-            for (int i = 0; i < E; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                adj.get(u).add(v); // Adding edge u -> v
-                adj.get(v).add(u); // Adding edge v -> u (undirected graph)
-            }
-
-            // Create Solution object and call bfsOfGraph
-            Solution obj = new Solution();
-            ArrayList<Integer> result = obj.bfsOfGraph(V, adj);
-
-            // Print the result
-            for (int node : result) {
-                System.out.print(node + " ");
-            }
-            System.out.println();
-        }
-
-        sc.close(); // Close the scanner
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> bfs = new ArrayList<>();
-        boolean[] visited = new boolean[V];
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int n = adj.size();
+        boolean visited[] = new boolean[n];
+        ArrayList<Integer> list = new ArrayList<>();
+        int src = 0;
+        Queue<Integer> q = new LinkedList<>();
+        visited[src] = true;
+        q.add(src);
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        visited[0] = true;
-        
-        while (!queue.isEmpty()) {
-            int vertex = queue.poll();
-            bfs.add(vertex);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            list.add(curr);
             
-            for (int neighbor : adj.get(vertex)) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
+            for(int x : adj.get(curr)){
+                if(!visited[x]){
+                    q.add(x);
+                    visited[x] = true;
                 }
             }
         }
-        return bfs;
+        return list;
     }
 }
