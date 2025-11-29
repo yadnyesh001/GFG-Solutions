540f28ec8a3278545fa0e75d0822476dc88fbe83
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-11-29 15:55:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int cutRod(int[] price) {
+        // code here
+        int n = price.length;
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], price[j - 1] + dp[i - j]);
+            }
+        }
+        return dp[n];
+    }
+}
